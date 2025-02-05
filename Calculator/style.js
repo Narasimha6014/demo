c11c6eb26fe7fd2b42f1f980d88a696e943ecd3d
Narasimha6014 <narasimhamanam6014@gmail.com>
2025-02-05 19:28:32 +0530

    Add files via upload

---
+++ Calculator/style.js
@@ -0,0 +1,33 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let buttonText = e.target.textContent;
+
+        if (buttonText === '=') {
+            try {
+                string = eval(string); 
+                input.value = string;
+            } catch {
+                input.value = "Error";
+                string = "";
+            }
+        } 
+        else if (buttonText === 'AC') {
+            string = "";
+            input.value = string;
+        } 
+        else if (buttonText === 'DEL') {
+            string = string.slice(0, -1); 
+            input.value = string;
+        } 
+        else {
+            string += buttonText;
+            input.value = string;
+        }
+    });
+});
